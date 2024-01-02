d07e660c59985ae17b8e64df52d220c2703b7a90
yoonahrim <user1@DESKTOP-VFP8CGU>
2024-01-02 14:22:52 +0900

    맴퍼와 컨트롤러와 중고리스트

--- ZangZack/src/main/java/com/kh/zangzac/yoonahrim/controller/SecondHandController.java
+++ ZangZack/src/main/java/com/kh/zangzac/yoonahrim/controller/SecondHandController.java
@@ -17,7 +17,7 @@ public class SecondHandController {
 	@Autowired
 	private secondHandService spService;
 	
-	
+	//중고 메인 페이지로 이동
 	@GetMapping("secondHand.ah")
 	public String secondHand() {
 		return "views/yoonahrim/secondHandList";
@@ -32,6 +32,9 @@ public class SecondHandController {
 	@PostMapping("update.ah")
 	public String updatePage(@ModelAttribute secondHandProduct sp, @RequestParam("spAddressStreet") String spAddressStreet, 
 			 @RequestParam("spAddressDetail") String spAddressDetail, Model model) {
+		
+		//로그인한 User의 게시물을 update 하기 위해
+		//로그인 유저의 id와 memebr_id가 일치할때 업도르 될 수 있도록 해야함
 		
 		String spAddress = null;
 		if(!spAddressStreet.equals("")) {
@@ -49,9 +52,11 @@ public class SecondHandController {
 			}
 	}
 	
-	//상세페이지
+	//중고 게시글 상세페이지
 	@GetMapping("detail.ah")
 	public String detailPage() {
+		//정보를 가져와서 list에 담은 다음 화면에 뿌리면 됨.
+		
 		return "views/yoonahrim/secondHandDetail";
 	}
 	
@@ -65,6 +70,9 @@ public class SecondHandController {
 	@PostMapping("/insert.ah")
 	public String insertPage(@ModelAttribute secondHandProduct sp, @RequestParam("spAddressStreet") String spAddressStreet, 
 							 @RequestParam("spAddressDetail") String spAddressDetail, Model model) {
+		//로그인한 User의 게시물을 insert 하기 위해
+		//로그인 유저의 id와 memebr_id가 일치할때 업도르 될 수 있도록 해야함
+		
 		String spAddress = null;
 		if(!spAddressStreet.equals("")) {
 			spAddress = spAddressStreet + " " + spAddressDetail;
@@ -81,13 +89,15 @@ public class SecondHandController {
 			}
 	}
 	
-	
+	//체크리스트 화면이동
 	@GetMapping("selectCategory.ah")
 	public String selectCategory() {
+		//화면 이동
+		
 		return "views/yoonahrim/selectCategory";
 	}
 	
-	
+	//채팅 화면 이동
 	@GetMapping("chating.ah")
 	public String chating() {
 		return "views/yoonahrim/chatingRoom";
