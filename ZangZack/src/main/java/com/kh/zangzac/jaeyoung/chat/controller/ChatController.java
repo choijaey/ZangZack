64455ca4b5e06a12ad3490e3f2dbc02a151ed9cb
PJiSung <starcr222@naver.com>
2024-01-05 17:48:30 +0900

    chat 70%

--- ZangZack/src/main/java/com/kh/zangzac/jaeyoung/chat/controller/ChatController.java
+++ ZangZack/src/main/java/com/kh/zangzac/jaeyoung/chat/controller/ChatController.java
@@ -2,6 +2,7 @@ package com.kh.zangzac.jaeyoung.chat.controller;
 
 import java.util.ArrayList;
 
+import org.json.simple.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,15 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kh.zangzac.common.ImageStorage;
+import com.kh.zangzac.jaeyoung.chat.ChatFileManager;
 import com.kh.zangzac.jaeyoung.chat.model.service.ChatService;
 import com.kh.zangzac.jaeyoung.chat.model.vo.ChatRoom;
+import com.kh.zangzac.jaeyoung.chat.model.vo.Chatter;
 
-import lombok.Getter;
-import lombok.Setter;
 
 @Controller
-@Getter
-@Setter
 public class ChatController {
 	
 	
@@ -32,6 +31,9 @@ public class ChatController {
         this.imageStorage = imageStorage;
     }
     
+    @Autowired
+    ChatFileManager cFileManager;
+    
     
     @GetMapping("chatListView.jy")
     public String chatListView(Model model) {
@@ -43,9 +45,22 @@ public class ChatController {
     }
     
     @GetMapping("/enterChatRoom.jy")
-    public String chatRoom(@RequestParam("chatRoomId") String roomName) {
+    public String chatRoom(@RequestParam("chatRoomId") String roomName,Model model) {
     	
+    	//채팅참가자 불러오기
+    	ArrayList<Chatter> list = cService.chatterList(roomName);
     	
+    	//채팅내역 불러오기
+    	//예시: "1" 방의 채팅 로그 읽어오기
+    	//unReadChatterCount
+        JSONArray chatLogs = cFileManager.readChatLog(roomName);
+        
+        
+        
+    	model.addAttribute("roomName", roomName);
+    	model.addAttribute("list", list);
+    	model.addAttribute("chatLogs", chatLogs);
+
     	
         //chatRoomId
         return "views/jaeyoung/chatRoomPage";
