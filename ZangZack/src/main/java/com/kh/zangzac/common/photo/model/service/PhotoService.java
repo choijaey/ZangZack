96f8764a1fb9156a96c1cb65ddfc0c96e877b8f1
PJiSung <starcr222@naver.com>
2024-01-08 15:45:14 +0900

    photo

---
+++ ZangZack/src/main/java/com/kh/zangzac/common/photo/model/service/PhotoService.java
@@ -0,0 +1,12 @@
+package com.kh.zangzac.common.photo.model.service;
+
+import java.util.ArrayList;
+
+import com.kh.zangzac.common.model.vo.SelectCondition;
+import com.kh.zangzac.common.photo.model.vo.Photo;
+
+public interface PhotoService {
+	
+	ArrayList<Photo> selectBoardPhoto(SelectCondition b);
+
+}
---
+++ ZangZack/src/main/java/com/kh/zangzac/common/photo/model/service/PhotoServiceImpl.java
@@ -0,0 +1,25 @@
+package com.kh.zangzac.common.photo.model.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.zangzac.common.model.vo.SelectCondition;
+import com.kh.zangzac.common.photo.model.dao.PhotoDAO;
+import com.kh.zangzac.common.photo.model.vo.Photo;
+
+@Service
+public class PhotoServiceImpl implements PhotoService{
+	
+	@Autowired
+	PhotoDAO pDAO;
+
+	@Override
+	public ArrayList<Photo> selectBoardPhoto(SelectCondition b) {
+		return pDAO.selectBoardPhoto(b);
+	}
+	
+	
+
+}
