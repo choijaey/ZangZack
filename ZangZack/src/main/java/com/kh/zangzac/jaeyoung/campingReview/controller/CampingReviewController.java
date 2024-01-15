5f829b3c73fb770b810471fbf1321ca79391254d
PJiSung <starcr222@naver.com>
2024-01-15 20:44:46 +0900

    commit

--- ZangZack/src/main/java/com/kh/zangzac/jaeyoung/campingReview/controller/CampingReviewController.java
+++ ZangZack/src/main/java/com/kh/zangzac/jaeyoung/campingReview/controller/CampingReviewController.java
@@ -1,21 +1,32 @@
 package com.kh.zangzac.jaeyoung.campingReview.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.zangzac.common.ImageStorage;
+import com.kh.zangzac.common.Pagination;
+import com.kh.zangzac.common.model.vo.PageInfo;
+import com.kh.zangzac.common.model.vo.SelectCondition;
 import com.kh.zangzac.common.photo.model.service.PhotoService;
 import com.kh.zangzac.common.photo.model.vo.Photo;
 import com.kh.zangzac.common.reply.model.service.ReplyService;
+import com.kh.zangzac.common.reply.model.vo.Reply;
 import com.kh.zangzac.jaeyoung.campingReview.model.service.CampingReviewService;
 import com.kh.zangzac.jaeyoung.campingReview.model.vo.CampingReview;
+import com.kh.zangzac.seongun.campboard.model.vo.CampBoard;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class CampingReviewController {
@@ -38,7 +49,31 @@ public class CampingReviewController {
 	
 	
 	@GetMapping("champingReviewListView.jy")
-	public String champingReviewListView() {
+	public String champingReviewListView(@RequestParam(value="page", defaultValue="1") int page,@RequestParam(value="searchText", defaultValue="") String searchText,Model model,HttpServletRequest request) {
+		
+		if(searchText==null) {
+			searchText="";
+		}
+		int listCount = crService.getListCount(searchText);
+		
+		int currentPage = page;
+		PageInfo pi = Pagination.getPageInfo(currentPage, listCount, 5);
+		ArrayList<CampingReview> list = crService.selectBoardList(pi,searchText);
+		
+		//사진들 가져오기
+		for(CampingReview cr : list) {
+			SelectCondition sc = new SelectCondition();
+		      sc.setBoardNo(cr.getCrNo());
+		      sc.setBoardType(8);
+		      ArrayList<Photo> pList = pService.selectBoardPhoto(sc);
+		      cr.setPhotos(pList);
+		      cr.setThumnail(pList.get(0)); //썸네일 넣기
+		}
+		
+		model.addAttribute("searchText", searchText);
+		 model.addAttribute("list", list);
+		 model.addAttribute("loc", request.getRequestURI());
+		 model.addAttribute("pi", pi);
 		
 		return "views/jaeyoung/champingRiewList";
 	}
@@ -95,6 +130,110 @@ public class CampingReviewController {
         pService.insertPhotoCampBoard(list);
 		
 		return "views/jaeyoung/champingRiewList";
+	}
+	
+	
+	@GetMapping("campingReviewDetail.jy")
+	public String campingReviewDetail(@RequestParam("crNo")int crNo,Model model) {
+		
+		CampingReview cr = crService.selectDetailCr(crNo);
+		model.addAttribute("cr", cr);
+		
+		// 사진 가져오기
+		  SelectCondition sc = new SelectCondition();
+	      sc.setBoardNo(crNo);
+	      sc.setBoardType(8);
+	      ArrayList<Photo> pList = pService.selectBoardPhoto(sc);
+	      cr.setPhotos(pList);
+	      
+	     
+	    //전체 리스트의 댓글들 저장하기
+	     ArrayList<Reply> replyList = rService.selectReply(sc);
+	     cr.setReplys(replyList);
+		
+		return "views/jaeyoung/champingReviewDetail";
+	}
+	
+	
+	
+	@GetMapping(value = "/campingReviewInsertReply.jy", produces = "application/json")
+	@ResponseBody
+	public Map<String, Object> insertReply(@ModelAttribute Reply reply) {
+		  
+		  reply.setBoardType(8);
+		  int result = rService.insertReply(reply);
+		  
+		  System.out.println(reply);
+		  Reply r = rService.selectReplyOne(reply);
+		  
+		  
+	      Map<String, Object> map = new HashMap<>();
+	      
+	      if(result >0) {
+	    	  map.put("replyNo", r.getReplyNo());
+	      }else {
+	    	  map.put("data", "no");
+	      }
+	      
+	       return map;
+	  }
+	
+	
+	@GetMapping(value = "/campingReviewUpdateReply.jy", produces = "application/json")
+	@ResponseBody
+	public Map<String, Object> campingReviewUpdateReply(@ModelAttribute Reply reply) {
+		  
+		  reply.setBoardType(8);
+		  int result = rService.updateReply(reply);
+		  
+		  
+	      Map<String, Object> map = new HashMap<>();
+	      
+	      if(result >0) {
+	    	  map.put("replyNo", reply.getReplyNo());
+	      }else {
+	    	  map.put("replyNo", "no");
+	      }
+	      
+	       return map;
+	  }
+	
+	
+	@GetMapping(value = "/campingReviewDeleteReply.jy", produces = "application/json")
+	@ResponseBody
+	public Map<String, Object> campingReviewDeleteReply(@ModelAttribute Reply reply) {
+		  
+		  reply.setBoardType(8);
+		  
+		 int result = rService.deleteReply(reply);
+		  
+	      Map<String, Object> map = new HashMap<>();
+	      
+	      if(result >0) {
+	    	  map.put("replyNo", reply.getReplyNo());
+	      }else {
+	    	  map.put("replyNo", "no");
+	      }
+	      
+	       return map;
+	  }
+	
+	
+	@GetMapping("campingReviewEdit.jy")
+	public String campingReviewEdit(@RequestParam("crNo")int crNo,Model model) {
+		
+		CampingReview cr = crService.selectDetailCr(crNo);
+		model.addAttribute("cr", cr);
+		
+		// 사진 가져오기
+		  SelectCondition sc = new SelectCondition();
+	      sc.setBoardNo(crNo);
+	      sc.setBoardType(8);
+	      ArrayList<Photo> pList = pService.selectBoardPhoto(sc);
+	      cr.setPhotos(pList);
+	      cr.setThumnail(pList.get(0));
+	      
+		return "views/jaeyoung/champingReviewEdit";
 	}
 
 }
--- ZangZack/src/main/java/com/kh/zangzac/jaeyoung/campingReview/model/service/CampingReviewServiceImpl.java
+++ ZangZack/src/main/java/com/kh/zangzac/jaeyoung/campingReview/model/service/CampingReviewServiceImpl.java
@@ -1,8 +1,12 @@
 package com.kh.zangzac.jaeyoung.campingReview.model.service;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.zangzac.common.model.vo.PageInfo;
 import com.kh.zangzac.jaeyoung.campingReview.model.dao.CampingReviewDAO;
 import com.kh.zangzac.jaeyoung.campingReview.model.vo.CampingReview;
 
@@ -15,6 +19,27 @@ public class CampingReviewServiceImpl implements CampingReviewService{
 	@Override
 	public int insertCampingReview(CampingReview cr) {
 		return crDAO.insertCampingReview(cr);
+	}
+
+	@Override
+	public ArrayList<CampingReview> selectList() {
+		return crDAO.selectList();
+	}
+
+	@Override
+	public int getListCount(String searchText) {
+		return crDAO.getListCount(searchText);
+	}
+
+	@Override
+	public ArrayList<CampingReview> selectBoardList(PageInfo pi, String searchText) {
+		RowBounds rowBounds = new RowBounds((pi.getCurrentPage()-1 )* pi.getBoardLimit(), pi.getBoardLimit());
+		return  crDAO.selectBoardList(rowBounds,searchText);
+	}
+
+	@Override
+	public CampingReview selectDetailCr(int crNo) {
+		return crDAO.selectDetailCr(crNo);
 	}
 
 }
