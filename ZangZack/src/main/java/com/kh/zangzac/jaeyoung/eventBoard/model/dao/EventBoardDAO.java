5f43bf6a73ca98298f9b09bf620e626db8db8dc9
PJiSung <starcr222@naver.com>
2024-01-16 17:50:30 +0900

    event board 50 %

--- ZangZack/src/main/java/com/kh/zangzac/jaeyoung/eventBoard/model/dao/EventBoardDAO.java
+++ ZangZack/src/main/java/com/kh/zangzac/jaeyoung/eventBoard/model/dao/EventBoardDAO.java
@@ -1,8 +1,21 @@
 package com.kh.zangzac.jaeyoung.eventBoard.model.dao;
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.RowBounds;
+
+import com.kh.zangzac.jaeyoung.eventBoard.model.vo.EventBoard;
 
 @Mapper
 public interface EventBoardDAO {
+
+	int insertEventBoard(EventBoard eb);
+
+	int getListCount();
+
+	ArrayList<EventBoard> selectEventBoardList(RowBounds rowBounds);
+
+	EventBoard selectEventBoard(int ebNo);
 
 }
--- ZangZack/src/main/java/com/kh/zangzac/jaeyoung/eventBoard/model/service/EventBoardService.java
+++ ZangZack/src/main/java/com/kh/zangzac/jaeyoung/eventBoard/model/service/EventBoardService.java
@@ -1,5 +1,18 @@
 package com.kh.zangzac.jaeyoung.eventBoard.model.service;
 
+import java.util.ArrayList;
+
+import com.kh.zangzac.common.model.vo.PageInfo;
+import com.kh.zangzac.jaeyoung.eventBoard.model.vo.EventBoard;
+
 public interface EventBoardService {
+
+	int insertEventBoard(EventBoard eb);
+
+	int getListCount();
+
+	ArrayList<EventBoard> selectEventBoardList(PageInfo pi);
+
+	EventBoard selectEventBoard(int ebNo);
 
 }
