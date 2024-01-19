0bb0c53a0aa37e8dced536dc017ea8268fc6313d
choijaey <154033841+choijaey@users.noreply.github.com>
2024-01-19 22:18:14 +0900

    Delete ZangZack - 복사본 (2) directory

--- ZangZack - 복사본 (2)/src/main/java/com/kh/zangzac/jaeyoung/campingReview/model/service/CampingReviewService.java
+++
@@ -1,9 +0,0 @@
-package com.kh.zangzac.jaeyoung.campingReview.model.service;
-
-import com.kh.zangzac.jaeyoung.campingReview.model.vo.CampingReview;
-
-public interface CampingReviewService {
-
-	int insertCampingReview(CampingReview cr);
-
-}
